e02b4c71caab8e7137237837dfbfe9cec6fad89d
clssw <838133633@qq.com>
2022-10-04 12:30:31 +0800

    init commit

---
+++ src/test/java/org/cuiwei/mdkid/MydearkidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cuiwei.mdkid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydearkidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
