746dd7b799da36e6d80f7055cc16a8e8eddf7139
clssw <838133633@qq.com>
2022-10-10 11:53:28 +0800

    修改yml配置，添加逆地理转换工具方法

---
+++ src/main/java/org/cuiwei/mdkid/constant/Constant.java
@@ -0,0 +1,5 @@
+package org.cuiwei.mdkid.constant;
+
+public class Constant {
+    public static final String GAODE_APPID="bbeee6a186f33f60ad3670dc3f9e706b";
+}
--- src/main/java/org/cuiwei/mdkid/util/CoordinateUtil.java
+++ src/main/java/org/cuiwei/mdkid/util/CoordinateUtil.java
@@ -1,15 +1,60 @@
 package org.cuiwei.mdkid.util;
 
+import org.cuiwei.mdkid.constant.Constant;
+
+import cn.hutool.core.text.StrFormatter;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CoordinateUtil {
-    //以字符串形式输入经纬度的转换
-    public static double convertToDecimalByString(String latlng){
+    /**
+     * 将度分秒格式经纬度字符串转换为小数格式
+     * 
+     * @param latlng
+     * @return
+     */
+    public static double convertToDecimalByString(String latlng) {
+        return convertToDecimalByString(latlng, "°", "'", "\"");
+    }
 
-        double du=Double.parseDouble(latlng.substring(0, latlng.indexOf("°")));
-        double fen=Double.parseDouble(latlng.substring(latlng.indexOf("°")+1, latlng.indexOf("'")));
-        double miao=Double.parseDouble(latlng.substring(latlng.indexOf("'")+1, latlng.indexOf("\"")));
-        if(du<0)
-            return -(Math.abs(du)+(fen+(miao/60))/60);
-        return du+(fen+(miao/60))/60;
+    /**
+     * 将度分秒格式经纬度字符串转换为小数格式
+     * 
+     * @param latlng
+     * @param d      度字符
+     * @param f      分字符
+     * @param m      秒字符
+     * @return
+     */
+    public static double convertToDecimalByString(String latlng, String d, String f, String m) {
+        double du = Double.parseDouble(latlng.substring(0, latlng.indexOf(d)));
+        double fen = Double.parseDouble(latlng.substring(latlng.indexOf(d) + 1, latlng.indexOf(f)));
+        double miao = Double.parseDouble(latlng.substring(latlng.indexOf(f) + 1, latlng.indexOf(m)));
+        if (du < 0)
+            return -(Math.abs(du) + (fen + (miao / 60)) / 60);
+        return du + (fen + (miao / 60)) / 60;
+    }
 
+    /**
+     * 根据经纬度信息获取地址
+     * 
+     * @param lng
+     * @param lat
+     * @return
+     */
+    public static String getAddressByCoordinate(double lng, double lat) {
+        String location = StrFormatter.format("{},{}", lng, lat);
+        String url = StrFormatter.format("https://restapi.amap.com/v3/geocode/regeo?key={}&location={}",
+                Constant.GAODE_APPID, location);
+        HttpResponse<JsonNode> res = Unirest.get(url).asJson();
+        if (res.isSuccess()) {
+            log.info("{}", res.getBody().toPrettyString());
+        } else {
+            return null;
+        }
+        return url;
     }
 }
