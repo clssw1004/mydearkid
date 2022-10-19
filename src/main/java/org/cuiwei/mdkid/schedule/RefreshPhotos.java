01e0dab7efee96a80edaca3d54dcac8cee81f981
clssw <838133633@qq.com>
2022-10-19 21:28:25 +0800

    备份

--- src/main/java/org/cuiwei/mdkid/schedule/RefreshPhotos.java
+++ src/main/java/org/cuiwei/mdkid/schedule/RefreshPhotos.java
@@ -1,5 +1,8 @@
 package org.cuiwei.mdkid.schedule;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import org.cuiwei.mdkid.entity.Photo;
 import org.cuiwei.mdkid.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +10,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class RefreshPhotos {
@@ -22,11 +29,17 @@ public class RefreshPhotos {
         if (!dir.exists() || !dir.isDirectory()) {
             return;
         }
+        List<Photo> photos = photoService.listAll();
+        Map<String, Photo> photoMap = new HashMap<>();
+        if (CollUtil.isNotEmpty(photos)) {
+            photoMap = photos.stream().collect(Collectors.toMap(Photo::getFileSha256, p -> p));
+        }
         String[] children = dir.list();
         for (String f : children) {
             try {
                 File file = new File(dir, f);
-                if (file.isFile() && !photoService.photoExist(file)) {
+                String sha256 = DigestUtil.sha256Hex(file);
+                if (file.isFile() && !photoMap.containsKey(sha256)) {
                     photoService.saveResource(file);
                 }
             } catch (Exception e) {
