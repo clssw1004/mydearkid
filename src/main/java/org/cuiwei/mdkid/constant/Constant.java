ccd707b5219c6dfcdbe16cb99f8b73463eeeae9b
clssw <838133633@qq.com>
2022-11-26 14:58:30 +0800

    删除高德key

--- src/main/java/org/cuiwei/mdkid/constant/Constant.java
+++ src/main/java/org/cuiwei/mdkid/constant/Constant.java
@@ -1,5 +1,6 @@
 package org.cuiwei.mdkid.constant;
 
+import cn.hutool.core.io.FileUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,16 +10,23 @@ import java.util.List;
 
 @Component
 public class Constant {
-    public static final String GAODE_APPID = "bbeee6a186f33f60ad3670dc3f9e706b";
+    public static  String GAODE_APPID;
 
     public static List<String> SUPPORT_EXTENSIONS = new ArrayList<>();
 
     public static final String ALL_TYPE_EXTENSION = "*";
 
+    @Value("${config.path.gaodekey}")
+    public void setGaodeAppid(String gaodekey) {
+        GAODE_APPID = FileUtil.readUtf8String(gaodekey);
+    }
+
     @Value("${config.EXTENSIONS:*}")
     public void setSupportExtensions(String extensions) {
         SUPPORT_EXTENSIONS = Arrays.stream(extensions.split(",")).map(ext -> ext.toUpperCase()).toList();
     }
+
+
 
 
 }
